dd006e4bd18116df5245fd2ce1e2a341ca6eeba7
Caleb Bron <cbron@users.noreply.github.com>
2019-09-17 12:55:10 -0700

    add logger

--- main.go
+++ main.go
@@ -9,11 +9,16 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
+	log.Println("request received")
 	time.Sleep(101 * time.Millisecond)
 	fmt.Fprintln(w, "Hi there, I'm running in Rio")
 }
 
 func main() {
+	log.SetPrefix("")
+	log.SetFlags(log.Lshortfile)
+	log.Println("init started")
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
